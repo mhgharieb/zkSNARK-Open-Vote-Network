666ee22c8d31132d6901b2bf0fb49c1fa5ed8fc4
Muhammad ElSheikh <eng.mohamed_gharieb@yahoo.com>
2022-10-28 21:29:57 +0200

    allow updating public keys & encrypted votes in 'original' & 'Sha256' designs

--- src/progressiveSha256/contracts/eVote.sol
+++ src/progressiveSha256/contracts/eVote.sol
@@ -52,10 +52,11 @@ contract eVote {
             bytes32[] memory _merkleProof
         ) public payable{
         require(msg.value==DEPOSIT,"Invalid deposit value");
-        require(voters.length + 1 <= nVoters, "Max number of voters is reached");
         require(block.number<finishRegistartionBlockNumber,"Registration phase is already closed");
-        require(vMerkleProof.verifyProof(_merkleProof, usersMerkleTreeRoot, keccak256(abi.encodePacked(msg.sender))), "Invalid Merkle proof");
         require(vzkSNARK.verifyProof(proof_a, proof_b, proof_c, _pubKey, 0),"Invalid DL proof");
+        require(publicKeys[msg.sender][0] == 0 && publicKeys[msg.sender][1] == 0, "Updating the public key is not allowed");
+        require(voters.length + 1 <= nVoters, "Max number of voters is reached");
+        require(vMerkleProof.verifyProof(_merkleProof, usersMerkleTreeRoot, keccak256(abi.encodePacked(msg.sender))), "Invalid Merkle proof");
         voters.push(msg.sender);
         publicKeys[msg.sender] = [_pubKey[0], _pubKey[1]];
         hashVotingKeysY = uint256(sha256(abi.encodePacked([hashVotingKeysY, _pubKey[1]]))) % SNARK_SCALAR_FIELD;
@@ -71,6 +72,8 @@ contract eVote {
         ) public {
         require(block.number >= finishRegistartionBlockNumber && block.number < finishVotingBlockNumber, "Voting phase is already closed");
         require( msg.sender == voters[_Idx], "Unregistered voter");
+        require(encryptedVotes[msg.sender][0] == 0 && encryptedVotes[msg.sender][1] == 0, "Updating the encrypted vote is not allowed");
+
         
 
         if (noHashOnesVotingKeysY == false){
